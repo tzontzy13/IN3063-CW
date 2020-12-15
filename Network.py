213465c99c49bbea76cc488b195eddfb0bc3064c
Cristi <chris54332@gmail.com>
2020-12-15 16:56:58 +0200

    added Network class and start

---
+++ Network.py
@@ -0,0 +1,146 @@
+from CrossEntropy import CrossEntropy
+from Dense import Dense
+from Relu import Relu
+from Sigmoid import Sigmoid
+import numpy as np
+
+# import matplotlib.pyplot as plt
+
+# Reference: Aayush Agrawal. 2020
+# Building Neural Network from scratch | by Aayush Agrawal | Towards Data Science.
+# [ONLINE] Available at: https://towardsdatascience.com/building-neural-network-from-scratch-9c88535bf8e9
+# [Accessed 15 December 2020]
+
+
+class Network():
+
+    def __init__(self):
+        self.network = []
+
+        # this is where you "build" your network structure
+        # Adds the sizes and types of layers used in the network
+        self.network.append(Dense(784, 200))
+        self.network.append(Relu())
+        self.network.append(Dense(200, 100))
+        self.network.append(Relu())
+        self.network.append(Dense(100, 10))
+
+        self.cross_entropy = CrossEntropy()
+
+    # Returns activations for all layers.
+    def forward(self, image_input):
+
+        prev_layer_activations = image_input
+        all_activations = []
+
+        # Performs the forward step by going through each layer's forward function
+        # Afterwards, stores the activation for the next iteration in the next layer
+        for layer in self.network:
+            # uses each layers forward function (example: relu and dense have a different one)
+            next_activation = layer.forward(prev_layer_activations)
+            all_activations.append(next_activation)
+            # set for next iteration
+            prev_layer_activations = all_activations[-1]
+
+        return all_activations
+
+    # Calculates the accuracy of the network by summing up the right-guessed answers
+    # It makes use of the output of the last activation and the targets
+    def accuracy(self, images, targets):
+
+        outputs = self.forward(images)
+
+        # stores the network output
+        y_actual = outputs[-1]
+
+        y_pred = []
+
+        for n in y_actual:
+            number = np.argmax(n)
+            y_pred.append(number)
+
+        acc_sum = 0
+        for pred, actual in zip(y_pred, targets):
+            if pred == actual:
+                acc_sum += 1
+
+        return acc_sum
+
+    # Trains the network by performing forward and backward pass on each mini-batch
+    def train(self, images, targets):
+
+        # Perform the forward pass on the given data. The last layer of the activations returns
+        # the Zs without any activation applied to it as it will be applied below when calculating the cost
+        activations = self.forward(images)
+        final_zs = activations[-1]
+        # Adds the mini-batch image inputs to be used later
+        all_inputs = [images] + activations
+
+        # Calculates the gradient of the last layer, at each neuron
+        # function from CrossEntropy class
+        gradient_l = self.cross_entropy.grad_softmax_crossentropy_with_logits(
+            final_zs, targets)
+        # median of all gradients for each image
+        grad = (np.sum(gradient_l) / len(gradient_l))
+
+        # This is where the backward pass starts.
+        # prepare inputs for backpropagation
+        # cant use np.flip because activations is an ndarray with multiple dimensions
+        flip_inputs = []
+        for i in range(len(activations) - 1, -1, -1):
+            flip_inputs.append(all_inputs[i])
+
+        # propagates the error on all layers
+        # using np.flip because here self.network is just a list of length (5)
+        for layer, input in zip(np.flip(self.network), flip_inputs):
+            # "go backwards" for each layer, calculating a new gradient dor that layer
+            gradient_l = layer.backward(input, gradient_l)
+
+        # get all losses - for each output neuron
+        all_losses = self.cross_entropy.softmax_cross_entropy_loss(
+            final_zs, targets)
+        # median of all losses for each image
+        loss = np.sum(all_losses / len(all_losses))
+
+        return loss, grad
+
+    # function for splitting dataset in minibatches
+    def split_data_in_batches(self, images, targets, minibatch_length):
+
+        # data structures for all images and targets
+        X_batches = []
+        y_batches = []
+
+        # shuffle starts here
+        # combine images with targets
+        mix = []
+        for image, target in zip(images, targets):
+            mix.append([image, target])
+
+        # shuflle mix
+        np.random.shuffle(mix)
+        mix2 = np.array(mix, dtype=object)
+
+        # data structures for shuffled images and targets
+        images3 = []
+        targets3 = []
+
+        # add shuffled images in targets to appropiate data structure
+        for i, t in mix2:
+            images3.append(i)
+            targets3.append(t)
+
+        # transform data so you have the same type as initial data passed to function
+        images3 = np.asarray(images3, dtype='float64')
+        targets3 = np.array(targets3, dtype='uint8')
+        # images3 and targets3 are shuffled data
+
+        #  append to minibach lists
+        for i in range(0, len(images), minibatch_length):
+
+            test_x = images3[i: i + minibatch_length]
+            test_y = targets3[i: i + minibatch_length]
+            X_batches.append(test_x)
+            y_batches.append(test_y)
+
+        return X_batches, y_batches
--- start.py
+++ start.py
@@ -1,23 +1,81 @@
+from get_data import load_dataset
 import numpy as np
-from mnist_data import get_mnist_data
 
-from MLP import MLP
-from MLP_SGD import MLP_SGD
+from Network import Network
 
-# get data from mnist_data file
-test_data, test_targets, train_data, train_targets = get_mnist_data()
+# you change the configuration for the network in the init of Network class
+print('\n')
+network = Network()
+print("Network initialized")
+print('\n')
 
-# initialize a Stochaistic Gradient Descent Multi Layer Perceptron
+# retrieve data
+X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
+print("Data has been loaded")
+print("Number of images in dataset: ", len(X_train) + len(X_val))
+print("Number of images to test on: ", len(X_test))
+print('\n')
 
-mlp_sgd = MLP_SGD(lr=0.25, sizes=[784, 30, 10],
-                  activation_list=['sigmoid', 'softmax'])
+# parameters for the network
+epochs = 5000
+minibatch_length = 32
 
-outputs1 = mlp_sgd.forward(test_data)
-print("Before update: ", mlp_sgd.loss(outputs1, test_targets))
-print("Accuracy: ", mlp_sgd.evaluate(test_data, test_targets))
+print("Number of epochs: ", epochs)
+print("Minibatch length: ", minibatch_length)
+print('\n')
 
-mlp_sgd.fit(train_data, train_targets, 5, 10)
+print("Epochs have started!")
+print('\n')
 
-outputs2 = mlp_sgd.forward(test_data)
-print("Loss after update: ", mlp_sgd.loss(outputs2, test_targets))
-print("Accuracy: ", mlp_sgd.evaluate(test_data, test_targets))
+# initial values for loss for each batch and accuracy
+loss_list_on_batches = []
+acc = 0
+
+# start program here
+# for each epoch
+for epoch in range(epochs):
+
+    print("Current epoch: ", epoch + 1)
+
+    # split data in minibatches
+    X_batches, y_batches = network.split_data_in_batches(X_train, y_train, minibatch_length = minibatch_length)
+
+    # for each minibatch
+    for X_batch, y_batch in zip(X_batches, y_batches):
+        # calculate batch loss
+        batch_loss, grad_for_stopping_criterion = network.train(X_batch, y_batch)
+        # append minibatch loss to list of all losses
+        loss_list_on_batches.append(batch_loss)
+
+    # calculate accuracy
+    acc_sum = network.accuracy(X_val, y_val)
+
+    # STOPPING CRITERION
+    # we decided to have the loss as our stopping criterion
+    # our loss can go up or down because the Categorical Cross-Entropy loss (WE DID OURSELVES) and gradient for our loss (WHICH WE COPIED - reference in file)
+    # are similar, but not dependent on each other, meaning:
+    # we did not use the Categorical Cross-Entropy loss funtion to compute the gradient
+    # BUT, it still works, the cost we calculated gets lower with each minibatch so we can compare it with the last minibatch
+    # if it doesnt change much, we stop training and print accuracy and loss
+    if(np.abs(loss_list_on_batches[-1] - loss_list_on_batches[-1 - minibatch_length]) < 0.005):
+        print("NN has saturated")
+        print("Accuracy:      ", acc_sum , "/", len(y_val))
+        print("Loss:          ", loss_list_on_batches[-1])
+        print('\n')
+        break
+
+    # if we havent reached saturation, we print this at the end of the last epoch
+    print("Accuracy:      ", acc_sum , "/", len(y_val))
+    print("Loss:          ", loss_list_on_batches[-1])
+    print('\n')
+#     plt.plot(train_log,label='train accuracy')
+#     plt.plot(val_log,label='val accuracy')
+#     plt.legend(loc='best')
+#     plt.grid()
+#     plt.show()
+
+# here we test on X_TEST and Y_TEST
+# we return the accuracy on the TEST DATA
+print("Testing")
+test_acc = network.accuracy(X_test, y_test)
+print("Test accuracy: ", test_acc, "/", len(y_test))
