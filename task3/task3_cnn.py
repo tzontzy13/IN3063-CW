396b292ee58acece20934cd3490f75ab0edcac4c
Cristi <chris54332@gmail.com>
2020-12-17 10:06:40 +0200

    pytorch networks for task 3

--- task3/task3_cnn.py
+++ task3/task3_cnn.py
@@ -3,13 +3,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.backends.cudnn
 import torchvision
-
-# source: https://nextjournal.com/gkoehler/pytorch-mnist
+import torch
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 # Hyper params sett
-
-
-n_epochs = 3
+n_epochs = 10
 batch_size_train = 64
 batch_size_test = 1000
 learning_rate = 0.01
@@ -20,23 +21,34 @@ random_seed = 1
 torch.backends.cudnn.enabled = False
 torch.manual_seed(random_seed)
 
-train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./task3data', train=True, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_train, shuffle=True)
+# train_loader = torch.utils.data.DataLoader(
+#     torchvision.datasets.MNIST('./task3data', train=True, download=True,
+#                                transform=torchvision.transforms.Compose([
+#                                    torchvision.transforms.ToTensor()
+#                                ])),
+#     batch_size=batch_size_train, shuffle=True)
 
+# test_loader = torch.utils.data.DataLoader(
+#     torchvision.datasets.MNIST('./task3data', train=False, download=True,
+#                                transform=torchvision.transforms.Compose([
+#                                    torchvision.transforms.ToTensor()
+#                                ])),
+#     batch_size=batch_size_test, shuffle=True)
+
+trainset = torchvision.datasets.MNIST('./task3data', train=True, download=True,
+                                      transform=torchvision.transforms.Compose([
+                                          torchvision.transforms.ToTensor()
+                                      ]))
+
+train_loader = torch.utils.data.DataLoader(
+    trainset, batch_size=batch_size_train, shuffle=True)
+
+testset = torchvision.datasets.MNIST('./task3data', train=False, download=True,
+                                     transform=torchvision.transforms.Compose([
+                                         torchvision.transforms.ToTensor()
+                                     ]))
 test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./task3data', train=False, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_test, shuffle=True)
+    testset, batch_size=batch_size_test, shuffle=True)
 
 
 class Net(nn.Module):
@@ -44,14 +56,15 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
+        # self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
         self.soft = nn.Softmax()
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        # x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
@@ -72,11 +85,16 @@ test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
 def train(epoch):
     network.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        optimizer.zero_grad()
         output = network(data)
+
+        # backward pass
         loss = F.nll_loss(output, target)
+        optimizer.zero_grad()
         loss.backward()
+
+        # update params
         optimizer.step()
+
         if batch_idx % log_interval == 0:
             # print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
             #     epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -85,8 +103,12 @@ def train(epoch):
             train_counter.append(
                 (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
 
-    print('Train Epoch {} finished, last mini-batch Loss: {:.6f}'.format(
+    print('Epoch {} Loss: {:.6f}'.format(
         epoch, train_losses[-1]))
+
+
+loss_list_on_epochs = []
+acc_list_on_epochs = []
 
 
 def test():
@@ -101,13 +123,67 @@ def test():
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
     test_loss /= len(test_loader.dataset)
-    test_losses.append(test_loss)
+    loss_list_on_epochs.append(test_loss)
+    acc_list_on_epochs.append(100. * correct / len(test_loader.dataset))
+    # test_losses.append(test_loss)
     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
-# test()
 for epoch in range(1, n_epochs + 1):
     train(epoch)
     test()
+    if abs(loss_list_on_epochs[-1] - loss_list_on_epochs[-2]) < 0.005:
+        print("Network Saturated")
+        loss_list_on_epochs = loss_list_on_epochs[1:]
+        break
+
+# Source: https://deeplizard.com/learn/video/0LhiS6yu2qQ
+
+
+@torch.no_grad()
+def get_all_preds(model, loader):
+    all_preds = torch.tensor([])
+    all_labels = torch.tensor([])
+    for batch in loader:
+        images, labels = batch
+
+        preds = model(images)
+        all_preds = torch.cat(
+            (all_preds, preds), dim=0
+        )
+        all_labels = torch.cat(
+            (all_labels, labels), dim=0
+        )
+    return all_preds, all_labels
+
+
+y_pred, y_test = get_all_preds(network, test_loader)
+y_pred = np.argmax(y_pred, axis=1)
+
+# PLOTS
+
+plt.plot(loss_list_on_epochs)
+plt.title("loss")
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['loss'], loc='best')
+plt.show()
+
+plt.plot(acc_list_on_epochs)
+plt.title("accuracy")
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['accuracy'], loc='best')
+plt.show()
+
+cm = confusion_matrix(y_test, y_pred)
+ax = plt.subplot()
+ax.set_title('Predicted vs Actual')
+ax.xaxis.set_ticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+ax.yaxis.set_ticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+sns.heatmap(cm, annot=True, ax=ax, cmap='Reds', fmt='g')
+plt.xlabel('Predicted labels', axes=ax)
+plt.ylabel('True labels', axes=ax)
+plt.show()
--- task3/task3_sgd.py
+++ task3/task3_sgd.py
@@ -1,110 +1,158 @@
-import numpy as np
-import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.backends.cudnn
 import torchvision
+import torch
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from time import time
-from torchvision import datasets, transforms
-from torch import nn, optim
+import numpy as np
+import seaborn as sns
 
-# Source: https://towardsdatascience.com/handwritten-digit-mnist-pytorch-977b5338e627
+# Hyper params
+n_epochs = 10
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.05
 
-transform = transforms.Compose([transforms.ToTensor()
-                                # transforms.Normalize((0.5,), (0.5,)),
-                                ])
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
 
-trainset = datasets.MNIST('./task3data', download=True,
-                          train=True, transform=transform)
-valset = datasets.MNIST('./task3data', download=True,
-                        train=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=64, shuffle=True)
-valloader = torch.utils.data.DataLoader(
-    valset, batch_size=64, shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('./task3data', train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor()
+                               ])),
+    batch_size=batch_size_train, shuffle=True)
 
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('./task3data', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor()
+                               ])),
+    batch_size=batch_size_test, shuffle=True)
 
-# print(images.shape)
-# print(labels.shape)
 
-input_size = 784
-hidden_sizes = [200, 100]
-output_size = 10
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(784, 200)
+        self.fc2 = nn.Linear(200, 100)
+        self.fc3 = nn.Linear(100, 10)
+        self.soft = nn.Softmax()
 
-model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[1], output_size),
-                      nn.CrossEntropyLoss()) #dim=1
-# print(model)
+        # self.conv2_drop = nn.Dropout2d()
 
-# criterion = nn.NLLLoss()
-criterion = nn.CrossEntropyLoss()
-images, labels = next(iter(trainloader))
-images = images.view(images.shape[0], -1)
+    def forward(self, x):
+        x = x.view(-1, 784)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.log_softmax(x)
 
-logps = model(images)  # log probabilities
-loss = criterion(logps, labels)  # calculate the NLL loss
 
-# print('Before backward pass: \n', model[0].weight.grad)
-# loss.backward()
-# print('After backward pass: \n', model[0].weight.grad)
+network = Net()
+optimizer = optim.SGD(network.parameters(),
+                      lr=learning_rate)
 
-optimizer = optim.SGD(model.parameters(), lr=0.03)  # , momentum=0.9
-time0 = time()
-epochs = 5
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
 
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
 
-        # Training pass
+def train(epoch):
+    network.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        output = network(data)
+
+        # backward pass
+        loss = F.nll_loss(output, target)
         optimizer.zero_grad()
-
-        output = model(images)
-        loss = criterion(output, labels)
-
-        # This is where the model learns by backpropagating
         loss.backward()
 
-        # And optimizes its weights here
+        # update params
         optimizer.step()
 
-        running_loss += loss.item()
-    else:
-        print("Epoch {} - Training loss: {}".format(e,
-                                                    running_loss/len(trainloader)))
+    print('Epoch {}'.format(epoch))
 
-print("\nTraining Time (in minutes) =", (time()-time0)/60)
 
-images, labels = next(iter(valloader))
+loss_list_on_epochs = [0]
+acc_list_on_epochs = []
 
-img = images[0].view(1, 784)
-with torch.no_grad():
-    logps = model(img)
 
-ps = torch.exp(logps)
-probab = list(ps.numpy()[0])
-print("Predicted Digit =", probab.index(max(probab)))
-# view_classify(img.view(1, 28, 28), ps)
+def test():
+    network.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = network(data)
+            test_loss += F.nll_loss(output, target,
+                                    size_average=False).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(test_loader.dataset)
+    loss_list_on_epochs.append(test_loss)
+    acc_list_on_epochs.append(100. * correct / len(test_loader.dataset))
+    # test_losses.append(test_loss)
+    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
-correct_count, all_count = 0, 0
-for images, labels in valloader:
-    for i in range(len(labels)):
-        img = images[i].view(1, 784)
-        with torch.no_grad():
-            logps = model(img)
 
-        ps = torch.exp(logps)
-        probab = list(ps.numpy()[0])
-        pred_label = probab.index(max(probab))
-        true_label = labels.numpy()[i]
-        if(true_label == pred_label):
-            correct_count += 1
-        all_count += 1
+for epoch in range(1, n_epochs + 1):
+    train(epoch)
+    test()
+    if abs(loss_list_on_epochs[-1] - loss_list_on_epochs[-2]) < 0.005:
+        print("Network Saturated")
+        loss_list_on_epochs = loss_list_on_epochs[1:]
+        break
 
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
+# Source: https://deeplizard.com/learn/video/0LhiS6yu2qQ
+
+
+@torch.no_grad()
+def get_all_preds(model, loader):
+    all_preds = torch.tensor([])
+    all_labels = torch.tensor([])
+    for batch in loader:
+        images, labels = batch
+
+        preds = model(images)
+        all_preds = torch.cat(
+            (all_preds, preds), dim=0
+        )
+        all_labels = torch.cat(
+            (all_labels, labels), dim=0
+        )
+    return all_preds, all_labels
+
+
+y_pred, y_test = get_all_preds(network, test_loader)
+y_pred = np.argmax(y_pred, axis=1)
+
+# PLOTS
+
+plt.plot(loss_list_on_epochs)
+plt.title("loss")
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['loss'], loc='best')
+plt.show()
+
+plt.plot(acc_list_on_epochs)
+plt.title("accuracy")
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['accuracy'], loc='best')
+plt.show()
+
+cm = confusion_matrix(y_test, y_pred)
+ax = plt.subplot()
+ax.set_title('Predicted vs Actual')
+ax.xaxis.set_ticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+ax.yaxis.set_ticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+sns.heatmap(cm, annot=True, ax=ax, cmap='Reds', fmt='g')
+plt.xlabel('Predicted labels', axes=ax)
+plt.ylabel('True labels', axes=ax)
+plt.show()
